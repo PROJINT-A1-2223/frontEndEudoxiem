0c500740edbcaa42f86e2605c4fba28c57d1dd42
eudoxiem <mwinkeudoxie@yahoo.fr>
2023-06-13 00:36:35 +0200

    creation du formulaire dans MessageSender

--- my-react-app/src/MessageSender.jsx
+++ my-react-app/src/MessageSender.jsx
@@ -1,8 +1,46 @@
 import React, { useState } from 'react';
-function MessageSender(){
-  return( 
-    <h1>
-      My Message
-    </h1>);
-}
 export default MessageSender;
+function MessageSender(){
+  const [message, setMessage] = useState('');
+  
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log('Message:', message);
+  const requestBody = {
+    message: message
+  };
+  fetch('http:localhost:8080/logMyMessage', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(requestBody)
+  })
+  .then((response) => response.json())
+      .then((data) => {
+        // Handle the response data here if needed
+      })
+      .catch((error) => {
+        // Handle any errors here
+      });
+    };        
+  return( 
+    <form onSubmit={handleSubmit}>
+      <p>
+        <label htmlFor="message">Message:</label>
+        <input
+          type="text"
+          id="message"
+          name="message"
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}
+        />
+      </p>
+      <p>
+      <button type="submit">Submit</button>
+      </p>
+    </form>
+  );
+  
+}
+
